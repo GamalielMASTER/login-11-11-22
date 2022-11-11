4828e69fa03ebd801541b5a9ffcfe3f1b2c1fff3
GamalielMASTER <dual.leon.solis@gmail.com>
2022-11-11 18:12:54 -0500

    creando un alista

---
+++ resources/views/lista.blade.php
@@ -0,0 +1,73 @@
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="(cc) 3con14" />
+    <title>Listas Ordenadas y desordenadas</title>
+</head>
+<body>
+    <h1>Listas de los alumnos</h1>
+    <ol>
+        <li>alumn 1</li>
+        <li>alumno 2</li>
+        <li>alumno 3</li>
+        <li>alumno 4</li>
+        <li>alumno 5</li>
+    </ol>
+    <h1>Lista de los alumnos registrados</h1>
+    <ul>
+        <li>Lunes
+            <ol>
+                <li>alumno 1</li>
+                    <ul type="square">
+                        <li>presente </li>
+                        <li>registrado</li>
+                    </ul>
+                
+            </ol>
+        </li>
+        <li>Martes
+            <ol>
+                <li>alumno 2</li>
+                    <ul type="square">
+                        <li>presente </li>
+                        <li>registrado</li>
+                    </ul>
+                </li>    
+            </ol>        
+        </li>
+        <li>miercoles
+            <ol>
+                <li>alumno 3</li>
+                    <ul type="square">
+                        <li>presente </li>
+                        <li>registrado</li>
+                    </ul>
+                </li>    
+            </ol>        
+        </li>
+        <li>jueves
+            <ol>
+                <li>alumno 4</li>
+                    <ul type="square">
+                        <li>presente </li>
+                        <li>registrado</li>
+                    </ul>
+                </li>    
+            </ol>        
+        </li>
+        <li>viernes
+            <ol>
+                <li>alumno 5</li>
+                    <ul type="square">
+                        <li>presente </li>
+                        <li>registrado</li>
+                    </ul>
+                </li>    
+            </ol>        
+        </li>
+    </ul>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/lista', function () {
+    return view('lista');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
